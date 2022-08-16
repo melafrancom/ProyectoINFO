9950ff481db0e805d15eb72eb6e2088cad4e5afb
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-16 01:23:57 -0300

    Creacion de modelos post/coment/categorias

--- src/apps/publicacion/admin.py
+++ src/apps/publicacion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .apps import PublicacionConfig
+from .models import Categoria, Coment, post
+
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Coment)
+admin.site.register(post)
+
--- src/apps/publicacion/models.py
+++ src/apps/publicacion/models.py
@@ -1,9 +1,29 @@
+from distutils.command.upload import upload
 from django.db import models
+from apps.accounts.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class post(models.Model):
     titulo = models.CharField("Titulo", max_length=250)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    img = models.ImageField(null=True, blank=True, upload_to='img/publicacion', help_text='Seleccione una imagen para mostrar:')
+    creado = models.DateTimeField(default=timezone.now)
+    modificado = models.DateField(auto_now=True)
+    publicado = models.DateField(blank=True, null=True)
+    categorias = models.ManyToManyField('Categoria', related_name='publicacion')
 
+
+class Coment(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(post, on_delete=models.CASCADE)
+    contenido = models.TextField("Contenido")
+    creado = models.DateTimeField(default=timezone.now)
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
 
 
