9f6eb30af038f02219b223b38fd5d8e76f574474
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-23 18:23:31 -0300

    intento deploy 3.0

--- src/HECHOS_CON_ALMA/asgi.py
+++ src/HECHOS_CON_ALMA/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HECHOS_CON_ALMA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HECHOS_CON_ALMA.settings.local')
 
 application = get_asgi_application()
--- src/HECHOS_CON_ALMA/settings/base.py
+++ src/HECHOS_CON_ALMA/settings/base.py
@@ -22,8 +22,8 @@ import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), 'static')
 ]
