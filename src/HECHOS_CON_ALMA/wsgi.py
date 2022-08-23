717863ce18766e4cdf1572e81b5392cd501e968a
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-23 19:35:13 -0300

    intento deploy 3.5

--- src/HECHOS_CON_ALMA/wsgi.py
+++ src/HECHOS_CON_ALMA/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HECHOS_CON_ALMA.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HECHOS_CON_ALMA.settings')
 
 application = get_wsgi_application()
