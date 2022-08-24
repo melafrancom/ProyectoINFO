bb9144b88d66a7190b2dab8daa29c3a9d885b26e
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-23 21:25:33 -0300

    modificaciones para deploy

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoINFO.src.HECHOS_CON_ALMA.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HECHOS_CON_ALMA.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
