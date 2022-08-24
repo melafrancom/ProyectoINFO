5358eb79cf4842326a7e34169a8c0cb2a13a76b0
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-23 23:46:43 -0300

    Agregado middleware

--- src/HECHOS_CON_ALMA/settings/base.py
+++ src/HECHOS_CON_ALMA/settings/base.py
@@ -65,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'HECHOS_CON_ALMA.urls'
