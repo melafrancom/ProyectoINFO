028ed9fefa65aff9ba0e7a7a76bdec1bf0db18aa
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-23 23:07:54 -0300

    Rehaciendo cambios

--- src/HECHOS_CON_ALMA/settings/base.py
+++ src/HECHOS_CON_ALMA/settings/base.py
@@ -126,11 +126,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-"""
+
 STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), 'static')
 ]
-"""
+
 
 MEDIA = 'media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
