42315307bd9e291294f62ff3bab5e547dfd49995
melitaf <107051646+melitaf@users.noreply.github.com>
2022-08-29 00:12:12 -0300

    primer pagina acoplada

--- src/HECHOS_CON_ALMA/views.py
+++ src/HECHOS_CON_ALMA/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def inicio(request):
-    template_name = "inicio.html"
+    template_name = "index.html"
     ctx = {}
     return render(request, template_name, ctx)
